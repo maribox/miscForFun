779f60460b497dc67b41a4209a665a019933a133
MySurmise <marlbos@outlook.de>
2021-03-23 18:35:59 +0100

    useful kappa

---
+++ euclid.py
@@ -0,0 +1,23 @@
+def stabprint(*args):
+    arglen = []
+    for arg in args:
+        arglen.append(len(str(arg)))
+    leng = max(arglen)
+    for arg in args:
+        print(arg, end = ((leng-len(str(arg))+1)*" " + "| "))
+        print("\u25a0"*arg)
+  
+    print("M" + leng * " " + "| ", end ="")
+    print("❐"*(int(args[0]/args[1])*args[1]), end ="")
+    print("▩"*int(args[0]%args[1]))
+    print("---------------------------------")
+
+def euclid(a, b):
+
+    if b == 0:
+        return a
+    else:
+        stabprint(a,b)
+        return euclid(b, a%b)
+
+print(euclid(150, 139))
