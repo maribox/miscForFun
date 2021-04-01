95a212d63ca0d15787843297e77776f8096ca6ee
MySurmise <marlbos@outlook.de>
2021-04-01 17:51:28 +0200

    mini Tool

--- NatReadFeat.py
+++ NatReadFeat.py
@@ -2,15 +2,22 @@ import pyautogui as pag
 from pynput import keyboard
 import time
 notReact = False
+playing = False
+
 
 def on_press(key):
     global notReact
+    global playing
     print(notReact)
-
     if not notReact:
         notReact = True
         if key == keyboard.Key.space:
-            pag.click(882, 60)
+            if pag.pixel(883, 59) == (227, 227, 227):
+                pag.click(882, 60)
+                playing = False
+            else:
+                pag.click(831, 61)
+                playing = True
             pag.hotkey("shift", "space")
             notReact = False
     else:
@@ -19,7 +26,7 @@ def on_press(key):
         
 
 def on_release(key):
-    if key == keyboard.Key.esc:
+    if key == keyboard.Key.esc: 
         # Stop listener
         listener.stop()
         return False
